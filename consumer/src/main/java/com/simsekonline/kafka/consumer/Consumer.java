a2c3483b9b218155cd02f6e18f4b9a9331274594
hayri şimşek <simsekha@hotmail.com>
2018-12-10 14:27:13 +0300

    kafka consumer added

---
+++ consumer/src/main/java/com/simsekonline/kafka/consumer/Consumer.java
@@ -0,0 +1,42 @@
+package com.simsekonline.kafka.consumer;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Consumer {
+	private static final String GROUP_ID = "my_consumer_app";
+	private static final String TOPIC = "second_topic";
+	static Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+	public static void main(String[] args) {
+
+		Properties properties = new Properties();
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+		consumer.subscribe(Collections.singleton(TOPIC));
+		while(true) {
+			ConsumerRecords<String,String> records= consumer.poll(Duration.ofMillis(100));
+			
+			for(ConsumerRecord<String, String> record:records) {
+				logger.info("--> "+record.key()+ "---"+record.value());
+			}
+		}
+	}
+
+}
